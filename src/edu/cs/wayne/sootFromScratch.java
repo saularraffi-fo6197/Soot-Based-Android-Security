26b6f0d201591c2e852288fb67cabe268133b4fb
Saular <fo6197@wayne.edu>
2019-11-14 00:32:19 -0500

    retrieve jimple body of methods. Rearanged code and created methods for generating classes, methods, and body content.

--- src/edu/cs/wayne/sootFromScratch.java
+++ src/edu/cs/wayne/sootFromScratch.java
@@ -2,12 +2,14 @@ package edu.cs.wayne;
 
 import java.util.Collections;
 import java.util.Iterator;
+import java.util.ArrayList;
 
 import soot.options.*;
 import soot.Scene;
 import soot.SootClass;
 import soot.SootMethod;
 import soot.jimple.JimpleBody;
+import soot.UnitPatchingChain;
 
 class sootFromScratch
 {
@@ -17,14 +19,18 @@ class sootFromScratch
 		this.apk_file_path = apk_path;
 	}
 	
-	public static void initializeSootOptions() {
+	// initialize soot options and set up Scene 
+	public static void initializeSoot() {
 		Options.v().set_src_prec(Options.src_prec_apk);
 		Options.v().set_verbose(false);
 		soot.options.Options.v().set_process_dir(Collections.singletonList(apk_file_path));
 	    soot.options.Options.v().set_allow_phantom_refs(true);
 	    soot.options.Options.v().set_force_android_jar("ic3-android.jar");
+	    
+	    Scene.v().loadNecessaryClasses();
 	}
 	
+	// generates a line with length of the string it is under 
 	public static void generateLine(String s) {
 		for (int i = 0; i < s.length(); i++) {
 			System.out.print("=");
@@ -32,34 +38,81 @@ class sootFromScratch
 		System.out.println("\n");
 	}
 	
+	// generates a list of classes in the APK
+	public static ArrayList<SootClass> getClasses() {
+		
+		ArrayList<SootClass> classes = new ArrayList<SootClass>();
+		
+		Iterator class_it = Scene.v().getApplicationClasses().iterator();
+		
+		while (class_it.hasNext()) 
+			classes.add((SootClass) class_it.next());
+		
+		return classes;
+	}
+	
+	// generates a list of methods in a given class 
+	public static ArrayList<SootMethod> getMethods(SootClass s_class) {
+		
+		ArrayList<SootMethod> methods = new ArrayList<SootMethod>();
+		
+		Iterator method_it = s_class.getMethods().iterator();
+		
+		while (method_it.hasNext()) 
+			methods.add((SootMethod) method_it.next());
+		
+		return methods;
+	}
+	
+	// generates the body of a given method 
+	public static ArrayList<UnitPatchingChain> getMethodBody(SootMethod method) {
+		
+		ArrayList<UnitPatchingChain> body = new ArrayList<UnitPatchingChain>();
+		
+		try {
+			JimpleBody s_body = (JimpleBody) method.retrieveActiveBody();
+			
+			Iterator chain_it = s_body.getUnits().iterator();
+			
+			while(chain_it.hasNext()) 
+				body.add((UnitPatchingChain) chain_it.next());
+		}
+		catch (RuntimeException e) {
+			
+		}
+		return body;
+	}
+	
+	// analyze the APK components 
 	public static void analyzeAPK() {
 				
 		System.out.println("Analyzing APK...\n");
 		
-		initializeSootOptions();
+		initializeSoot();
 		
-		Scene.v().loadNecessaryClasses();
-		
-		Iterator class_it = Scene.v().getApplicationClasses().iterator();
-		
-		while (class_it.hasNext()) {
+		for (SootClass s_class : getClasses()) {
 			
-			SootClass s_class = (SootClass) class_it.next();
-			Iterator method_it = s_class.getMethods().iterator();
+			if (s_class.getName() == "com.karmacracy.app.ui.RegisterActivity") {
 			
-			System.out.println("\n\n\n\n" + s_class.getName());
-			generateLine(s_class.getName());
-			
-			while (method_it.hasNext()) {
+				System.out.println("\n\n" + s_class.getName());
+				generateLine(s_class.getName());
 				
-				SootMethod method = (SootMethod) method_it.next();
-				System.out.println("  *  " + method.getSignature());
-				
-				try {
-					JimpleBody body = (JimpleBody) method.retrieveActiveBody();
-					// System.out.println(body.getLocals());
+				for (SootMethod method : getMethods(s_class)) {
+					System.out.println("  *  " + method.getSignature() + "\n");
+					
+					try {
+						JimpleBody body = (JimpleBody) method.retrieveActiveBody();
+						UnitPatchingChain chain = body.getUnits();
+						Iterator chain_it = body.getUnits().iterator();
+						while(chain_it.hasNext()) {
+							System.out.println("\t# " + chain_it.next());
+						}
+						System.out.println("\n");
+					}
+					catch (RuntimeException e) {
+						System.out.println("Error");
+					}
 				}
-				catch (RuntimeException e) {}
 			}
 		}
 	}
